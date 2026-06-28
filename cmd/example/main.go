f4a035ba5cb6041c915d78a903f3c1f34ef3585f
Yulia-GG <ggjulia777@gmail.com>
2026-06-28 17:18:43 +0600

    Update main.go

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"example-solid/internal/repository/sqlite3"
-    "example-solid/internal/notifier"
-    "example-solid/internal/service"
 	"database/sql"
+	"example-solid/internal/notifier"
+	"example-solid/internal/repository/sqlite3"
+	"example-solid/internal/service"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -17,7 +17,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//defer db.Close()
+	defer db.Close() // закрываем б.д. после открытия
 
 	// создаем репозиторий (первый вариант)
 	// repo := &sqlite3.SQLiteRepo{
@@ -27,22 +27,36 @@ func main() {
 	// (второй вариант - через конструктор)
 	repo := sqlite3.NewSQLiteRepo(db)
 
-	// создание системы
-	service := service.NewOrderService(
+	// создание SMS системы
+	SMSService := service.NewOrderService(
 		repo,
-		&notifier.EmailSender{},
+		&notifier.SMSSender{},
 		repo,
 	)
 
 	// создание таблицы
-	err = service.CreateTable()
+	err = SMSService.CreateTable()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Пример использования
-	err = service.ProcessAndNotify("Иван", []string{"apple", "banana"}, 10.5)
+	err = SMSService.ProcessAndNotify("Иван", []string{"apple", "banana"}, 10.5)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// создание Email системы
+EmailService := service.NewOrderService(
+		repo,
+		&notifier.EmailSender{},
+		repo,
+	)
+
+// Пример использования
+	err = EmailService.ProcessAndNotify("Николай", []string{"apple", "mango"}, 15.5)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
